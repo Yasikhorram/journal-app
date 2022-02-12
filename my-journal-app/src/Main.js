5ece2b9815bc538c8cf7a9f393b6d05b487f3a5f
Yasikhorram <khorramyasaman@gmail.com>
2022-02-11 23:03:41 -0400

    Use material ui button

--- my-journal-app/src/Main.js
+++ my-journal-app/src/Main.js
@@ -1,5 +1,13 @@
+import Button from "@mui/material/Button";
+
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        Add notes
+      </Button>
+    </div>
+  );
 };
 
 export default Main;
