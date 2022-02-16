19bc849b906732b2f596d14ff2346e803237d2f8
Yasikhorram <khorramyasaman@gmail.com>
2022-02-15 20:18:54 -0400

    Handle side bar

--- my-journal-app/src/App.js
+++ my-journal-app/src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 import { useState } from "react";
+import uuid from "react-uuid";
 
 function App() {
   const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled note",
+      body: "",
+      lastModified: Date.now(),
+    };
+    setNotes([newNote, ...notes]);
+  };
   return (
     <div className="App">
-      <Sidebar notes={notes} />
+      <Sidebar notes={notes} onAddNote={onAddNote} />
       <Main />
     </div>
   );
--- my-journal-app/src/Sidebar.js
+++ my-journal-app/src/Sidebar.js
@@ -1,19 +1,21 @@
-const Sidebar = ({ notes }) => {
+const Sidebar = ({ notes, onAddNote }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>NOTES</h1>
-        <button>Add</button>
+        <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
         {notes.map((note) => (
           <div className="app-sidebar-note">
             <div className="sidebar-note-title">
-              <strong>TITLE</strong>
+              <strong>{note.title}</strong>
               <button>Delete</button>
             </div>
-            <p>Note preview</p>
-            <small className="note-meta">Last Modified [date]</small>
+            <p> {note.body && note.body.substr(0, 100) + "..."} </p>
+            <small className="note-meta">
+              Last Modified [{new Date().toLocaleDateString()}]
+            </small>
           </div>
         ))}
       </div>
@@ -22,3 +24,10 @@ const Sidebar = ({ notes }) => {
 };
 
 export default Sidebar;
+
+// Explanation:
+// -> note is an obj because our notes state is an arr of objs
+// -> We dont want to show the whole body <p>{note.body}</p> So we do conditional
+//rendering. So if theres a note.body do this othrwise do nothin and we write it with
+//&& <p> {note.body && note.body.substr(0, 100) + "..."} </p>
+// -> note.lastModified to make this readable we use new Date()
